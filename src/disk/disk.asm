48c7dae02ade328a0a39dc374f722515cea8c4ca
swisd <glordi053@gmail.com>
2025-11-09 23:21:00 -0500

    things

---
+++ src/disk/disk.asm
@@ -0,0 +1,9 @@
+section .data
+DISK            DB   0   ;Disk Number
+DISK_CYLINDER   DQ   0   ;Disk Cylinder
+DISK_HEAD       DB   0   ;Disk Head
+DISK_SECTOR     DQ   0   ;Disk Sector
+
+NUM_SECT        DB   0
+
+SECTOR_LOAD     DQ   0x7e00
---
+++ src/mem/paging/page.asm
@@ -0,0 +1,6 @@
+section .data
+
+SEGMENT_WIDTH  DQ  65536  ;64K
+
+section .text
+
