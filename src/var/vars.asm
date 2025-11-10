444126e3b1c78866c3cae9674ef7de11ccf47ddf
swisd <glordi053@gmail.com>
2025-11-09 22:35:14 -0500

    👍

--- src/var/vars.asm
+++ src/var/vars.asm
@@ -1,8 +1,19 @@
 section .data
 
-VGA_START   DB     0xb8000   ;VGA zone start
-DMX_START   DB     0xf0000   ;DMX zone start
+ZEROADDR    DQ     0x0000000000000000
+ENDADDR     DQ     0xffffffffffffffff
+
+VGA_START   DW     0xb8000   ;VGA zone start
+DMX_START   DW     0xf0000   ;DMX zone start
 
 BITMODE     DB     64        ;64-bit mode
 
-REG_ADDR    DB     0xff000   ;Registry address
+REG_ADDR    DW     0xff000   ;Registry address (hopefully)
+
+
+BUF_WIDTH   DB     32        ;Command Buffer width
+BUF_HEIGHT  DW     9001      ;Command Buffer height
+
+
+ZMAX        DD     255
+ZMIN        DD     0
